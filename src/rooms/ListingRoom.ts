edfb34bcbaa1ffbed84d85950114f2f64bbca2d0
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-12-16 13:44:34 +0700

    Implement listing service events

--- src/rooms/ListingRoom.ts
+++ src/rooms/ListingRoom.ts
@@ -1,14 +1,18 @@
 import { Room, Client, ServerError } from "colyseus";
 import http from "http";
 import winston from "winston";
-import { getLogger } from "..";
+import { getLogger, getListingService } from "..";
+import { ListingService } from "../services/ListingService";
 import { ListingRoomState } from "./schema/ListingRoomState";
 
 export class ListingRoom extends Room<ListingRoomState> {
   private logger: winston.Logger;
+  private listingService: ListingService;
 
   onCreate(options: any) {
     this.logger = getLogger();
+    this.listingService = getListingService();
+    this.listingService.onCreateRoom(this);
     this.setState(new ListingRoomState());
 
   }
@@ -21,10 +25,12 @@ export class ListingRoom extends Room<ListingRoomState> {
   }
 
   onJoin(client: Client, options: any) {
+    this.listingService.onConnect(client, options);
     this.logger.info(`[broadcast] ${client.sessionId} joined!`);
   }
 
   onLeave(client: Client, consented: boolean) {
+    this.listingService.onDisconnect(client);
     this.logger.info(`[broadcast] ${client.sessionId} left!`);
   }
 
